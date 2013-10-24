3e28d8e787e5a509353d1921719cfcde69b3f00a
Jeff Doan <jeff@goodwurk.com>
2013-10-24 16:54:11 -0400

    Update CSS to include print CSS for lists

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,4 @@
-var messages = ["are <strong>The Black Keys</strong>", "is <strong>Miley Cyrus</strong>", "is <strong>50 Cent</strong>", "are <strong>Ween</strong>", "are <strong>Lez Zeppelin</strong>"];
+var messages = ['When are <strong>The Black Keys</strong> coming to town next? Where will they be <strong class="r">next month?</strong>', 'When is <strong>Miley Cyrus</strong> coming to town next? Where will she be <strong class="r">next month?</strong>', 'When is <strong>50 Cent</strong> coming to town next? Where will he be <strong class="r">next month?</strong>', 'When are <strong>Lez Zeppelin</strong> coming to town next? Where will they be <strong class="r">next month?</strong>'];
 function getMessage() {
    return messages[Math.floor(Math.random() * messages.length)];
 }
